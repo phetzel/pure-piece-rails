03f026d44094449e42c1b22a14423e085535ab8f
Phillip <phillip@Phillips-MacBook-Air.local>
2023-03-10 13:34:43 -0800

    Added subscribe to email for checkout

--- app/controllers/api/v1/checkout_controller.rb
+++ app/controllers/api/v1/checkout_controller.rb
@@ -1,13 +1,32 @@
 class Api::V1::CheckoutController < ApplicationController
     def create
+        # client_reference_id - checkout session id 
+        # customer - customer id 
+        # customer_email - if already has email
+        # automatic_tax
+        # custom_fields
+
         session = Stripe::Checkout::Session.create({
             line_items: JSON.parse(checkout_params[:items]),
             mode: 'payment',
-            success_url: 'http://localhost:3000?success=true',
-            cancel_url: 'http://localhost:3000',
+            success_url: 'http://localhost:3000?checkout=1',
+            cancel_url: 'http://localhost:3000?checkout=0',
             shipping_address_collection: {
                 allowed_countries: ['US']
-            }
+            },
+            custom_fields: [
+                {
+                    key: 'subscribe',
+                    label: 'Subscribe to Mailing List?',
+                    type: 'dropdown',
+                    dropwdown: {
+                        options: [
+                            { label: "Yes", value: true },
+                            { label: "No", value: false },
+                        ]
+                    }
+                }
+            ]
           })
         
           render json: { redirect_url: session.url }, status: :ok
